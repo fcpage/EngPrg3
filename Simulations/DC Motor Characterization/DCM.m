f7a7081dd8867c7a461840fcadb6e206feed6778
fcpage <158251261+fcpage@users.noreply.github.com>
2025-11-19 02:18:25 -0500

    Project 3
    Scope Shots
    `

---
+++ Simulations/DC Motor Characterization/DCM.m
@@ -0,0 +1,20 @@
+R=2.8;
+L=0.0017342;
+J=0.0000001343;
+b=0.1;
+Ke=0.6429;
+Kt=0.6341;
+K=1.19;
+
+syms s;
+tA=1;
+tB=(R*J+b*L)/(L*J);
+tC=(R*b+Ke*Kt)/(L*J);
+tD=Kt/(L*J);
+tG=2.376;
+
+T1=tG*tf([tD], [tA tB tC])
+C
+zpk(C)
+T2=(tG*C)/(1+(tG*C))
+zpk(T2)
---
+++ Simulations/DC Motor Characterization/ScopePlotter.m
@@ -0,0 +1,16 @@
+f="step.csv";
+opts = detectImportOptions(f);
+T = readtable(f, opts);
+A = table2array(T);
+
+I = polyfit(A(1,:), A(2,:), 1);
+O = polyfit(A(1,:), A(3,:), 1);
+H = tf(O,I)
+
+subplot(2,1,1);
+plot(T,"X", "CH1")
+title('Step Input (V vs. s)')
+
+subplot(2,1,2); 
+plot(T, "X", "CH2")
+title('DC Motor Step Response (V vs. s)')
